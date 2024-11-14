3ddbfc53cb693668581f20da9ce1c5285949c1e8
why <3353620592@qq.com>
2024-11-14 18:41:18 +0800

    feat：静态页面

---
+++ src/app/components/checkComment/index.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import styles from "./index.module.scss";
+import React from 'react';
+import camera from '../../../asset/camera.png';
+import BackGround from '../../../asset/postsBackGround.png';
+import otherImg1 from '../../../asset/otherImg1.png';
+import userImg from '../../../asset/userImg.png';
+
+
+export const CheckComment = () => {
+
+
+  return (
+    <div className={styles.content}>
+      <div className={styles.commentList}>
+        <div className={styles.comment}>
+          <div className={styles.userImg}>
+            <Image src={otherImg1} alt="" />
+          </div>
+          <div className={styles.userInfo}>
+            <div className={styles.userName}>
+              用户名称
+            </div>
+            <div className={styles.postTime}>
+              评论了你的帖子
+            </div>
+            <div className={styles.postContent}>
+              学习障碍（Learning Disabilities, LD）是指在听、说、读、写、推理和数学等方面的特定困难。
+              这些障碍通常是先天性的，并且可能会影响儿童在学校的表现和日常生活中的学习能力。
+            </div>
+          </div>
+        </div>
+        <div className={styles.comment}>
+          <div className={styles.userImg}>
+            <Image src={otherImg1} alt="" />
+          </div>
+          <div className={styles.userInfo}>
+            <div className={styles.userName}>
+              用户名称
+            </div>
+            <div className={styles.postTime}>
+              评论了你的帖子
+            </div>
+            <div className={styles.postContent}>
+              学习障碍（Learning Disabilities, LD）是指在听、说、读、写、推理和数学等方面的特定困难。
+              这些障碍通常是先天性的，并且可能会影响儿童在学校的表现和日常生活中的学习能力。学习障碍（Learning Disabilities, LD）是指在听、说、读、写、推理和数学等方面的特定困难。
+              这些障碍通常是先天性的，并且可能会影响儿童在学校的表现和日常生活中的学习
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,15 @@ import React from 'react';
 import { usePathname, useParams, useRouter } from 'next/navigation';
 import logo from '../asset/logo.png';
 import childrenPage from '../asset/children.png';
+import leftUnder from '../asset/leftUnder.png';
+import leftUp from '../asset/leftUp.png';
+import rightUnder from '../asset/rightUnder.png';
+import rightUp from '../asset/rightUp.png';
 import { CodeLogin } from '../app/components/CodeLogin'
 import { Registration } from '../app/components/Registration'
+import { PersonalPage } from '../app/components/personalPage'
+import { PostsCommunication } from '../app/components/postsCommunication'
+import { ShareExperiencePage } from '../app/components/shareExperiencePage'
 
 export default function Home() {
   const router = useRouter();
@@ -33,22 +40,33 @@ export default function Home() {
             </div>
             <div className={styles.rightContent}>
               <div className={styles.introduction}>
-                <p>引语：</p>
-                <p>智启无碍交流论坛上线啦！</p>
-                <p>无论你想倾诉、求助，还是分享学困同行的故事，探讨具体问题，甚至只想感慨吐槽几句……都可以写下来发布帖子。我们都会用心倾听，并调动网站所有资源，合作机构的专业特教老师、心理咨询师……各尽其能，提供建议和帮助。
-                  如果你的帖子能给其他用户启发与借鉴，我们也会编辑发表在“智启”专栏。
-                </p>
-                <p>愿“智启论坛”可以与你同行，陪伴你走过所有的时光。</p>
+                <div className={styles.introduce}>
+                  <p>引语：</p>
+                  <p>智启无碍交流论坛上线啦！</p>
+                  <p>无论你想倾诉、求助，还是分享学困同行的故事，探讨具体问题，甚至只想感慨吐槽几句……都可以写下来发布帖子。我们都会用心倾听，并调动网站所有资源，合作机构的专业特教老师、心理咨询师……各尽其能，提供建议和帮助。
+                    如果你的帖子能给其他用户启发与借鉴，我们也会编辑发表在“智启”专栏。
+                  </p>
+                  <p>愿“智启论坛”可以与你同行，陪伴你走过所有的时光。</p>
+                </div>
+                <Image src={leftUnder} alt="" className={styles.leftUnder} />
+                <Image src={leftUp} alt="" className={styles.leftUp} />
+                <Image src={rightUnder} alt="" className={styles.rightUnder} />
+                <Image src={rightUp} alt="" className={styles.rightUp} />
               </div>
             </div>
           </>
         )
-      default:
+      case 1:
         return (
-          <>
-
-            
-          </>
+          <PersonalPage />
+        )
+      case 2:
+        return (
+          <PostsCommunication />
+        )
+      case 3:
+        return (
+          <ShareExperiencePage />
         )
     }
   }, [pagePart]);
