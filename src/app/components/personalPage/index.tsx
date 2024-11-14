2a108a64e336835c912545615d40e897e8528dcf
why <3353620592@qq.com>
2024-11-14 18:42:12 +0800

    feat：静态页面

--- src/app/components/personalPage/index.tsx
+++ src/app/components/personalPage/index.tsx
@@ -1,23 +1,57 @@
 import Image from "next/image";
 import styles from "./index.module.scss";
 import React from 'react';
+import userImg from '../../../asset/userImg.png';
 
 
 export const PersonalPage = () => {
   return (
     <>
       <div className={styles.leftContent}>
-        {/* <Image src={logo} alt="Logo" className={styles.mainLogo} />
-        <Image src={childrenPage} alt="leftMainContent" className={styles.mainContent} /> */}
+        <div className={styles.userName}>
+          用户名称
+        </div>
+        <div className={styles.userImg}>
+          <Image src={userImg} alt="" />
+        </div>
+        <div className={styles.userId}>
+          账号
+        </div>
+        <div className={styles.userStage}>
+          儿童学段
+        </div>
+        <button className={styles.changeInformation}>
+          点此修改个人信息
+        </button>
       </div>
       <div className={styles.rightContent}>
-        <div className={styles.introduction}>
-          <p>引语：</p>
-          <p>智启无碍交流论坛上线啦！</p>
-          <p>无论你想倾诉、求助，还是分享学困同行的故事，探讨具体问题，甚至只想感慨吐槽几句……都可以写下来发布帖子。我们都会用心倾听，并调动网站所有资源，合作机构的专业特教老师、心理咨询师……各尽其能，提供建议和帮助。
-            如果你的帖子能给其他用户启发与借鉴，我们也会编辑发表在“智启”专栏。
-          </p>
-          <p>愿“智启论坛”可以与你同行，陪伴你走过所有的时光。</p>
+        <div className={styles.title}>
+          我发布的帖子
+        </div>
+        <div className={styles.postsContent}>
+          <div className={styles.posts}>
+            <p>
+              学习障碍（Learning Disabilities, LD）是指在听、说、读、写、推理和数学等方面的特定困难。
+              这些障碍通常是先天性的，并且可能会影响儿童在学校的表现和日常生活中的学习能力。
+              值得注意的是，学习障碍并不等同于智力障碍，许多学习障碍儿童在其他领域可能表现出色。
+              学习障碍（Learning Disabilities, LD）是指在听、说、读、写、推理和数学等方面的特定困难。
+              这些障碍通常是先天性的，并且可能会影响儿童在学校的表现和日常生活中的学习能力。
+              值得注意的是，学习障碍并不等同于智力障碍，许多学习障碍儿童在其他领域可能表现出色。
+              学习障碍（Learning Disabilities, LD）是指在听、说、读、写、推理和数学等方面的特定困难。
+              这些障碍通常是先天性的，并且可能会影响儿童在学校的表现和日常生活中的学习能力。
+              值得注意的是，学习障碍并不等同于智力障碍，许多学习障碍儿童在其他领域可能表现出色。
+              并且可能会影响儿童在学校的表现和日常生活中的学习能力。
+              值得注意的是，学习障碍并不等同于智力障碍，许多学习障碍儿童在其他领域可能表现出色。
+            </p>
+          </div>
+          <div className={styles.posts}>
+            学习障碍（Learning Disabilities, LD）是指在听、说、读、写、推理和数学等方面的特定困难。
+            这些障碍通常是先天性的，并且可能会影响儿童在学校的表现和日常生活中的学习能力。
+            值得注意的是，学习障碍并不等同于智力障碍，许多学习障碍儿童在其他领域可能表现出色。
+            学习障碍（Learning Disabilities, LD）是指在听、说、读、写、推理和数学等方面的特定困难。
+            这些障碍通常是先天性的，并且可能会影响儿童在学校的表现和日常生活中的学习能力。
+            值得注意的是，学习障碍并不等同于智力障碍，许多学习障碍儿童在其他领域可能表现出色。
+          </div>
         </div>
       </div>
     </>
