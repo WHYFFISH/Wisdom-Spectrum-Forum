3459662348507e62f85e122452fc33c079295fb7
why <3353620592@qq.com>
2024-11-27 21:35:22 +0800

    1

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // 启用静态导出模式
+  basePath: "/Wisdom-Spectrum-Forum", // 设置基础路径
+  assetPrefix: "/Wisdom-Spectrum-Forum/", // 设置资源前缀
 };
 
 export default nextConfig;
